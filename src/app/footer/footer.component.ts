4ae214585e1cdc056d5477f8ed649d7ec478d7c2
Olbor-dev <olbor.dev@gmail.com>
2025-04-07 15:13:17 +0200

    mise en forme scss footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,14 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   template: `
-    <div>
+    <div class="myContainer">
       <div class="rectangle">
 
       </div>
       <div class="footer">
         <div class="info">
-          <img src="Logo-TMA-300px.png" alt="Logo du site trouver mon artisan de la région Auvergne Rhône Alpes">
-          <address>
+          <img class="logo" src="assets/images/Logo-White-500px.png" alt="Logo du site trouver mon artisan de la région Auvergne Rhône Alpes">
+          <address class="address">
             <h3><strong>Lyon</strong></h3>
             <p>
               101 cours Charlemagne<br>
@@ -19,17 +19,17 @@ import { Component } from '@angular/core';
               69269 LYON CEDEX 02<br>
               France
             </p>
-            <div class="telephone">
-              <img src="telephone.svg" alt="Logo téléphone">
-              <a href="tel:+33426734000">+ 33(0)4 26 73 40 00</a>
+            <div class="telephone nav-underline">
+              <img src="telephone-white.svg" alt="Logo téléphone" class="telephone-logo">
+              <a class="nav-link" href="tel:+33426734000">+ 33(0)4 26 73 40 00</a>
             </div>
           </address>
         </div>
-        <div class="links">
-          <a href="">Mentions légales</a>
-          <a href="">Données personnelles</a>
-          <a href="">Accessibilité</a>
-          <a href="">Cookies</a>
+        <div class="menu nav-underline">
+          <a class="nav-link" href="#">Mentions légales</a>
+          <a class="nav-link" href="#">Données personnelles</a>
+          <a class="nav-link" href="#">Accessibilité</a>
+          <a class="nav-link" href="#">Cookies</a>
         </div>      
       </div>
     </div>
